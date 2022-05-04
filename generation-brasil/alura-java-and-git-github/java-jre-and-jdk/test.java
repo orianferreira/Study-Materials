80ebdd236f1142df1a4311a3c94ba43cd6a91778
Rian De Almeida Ferreira <64613463+Aerinho@users.noreply.github.com>
2022-05-04 13:48:49 -0300

    Commit 05/04/2022

---
+++ generation-brasil/alura-java-and-git-github/java-jre-and-jdk/test.java
@@ -0,0 +1,11 @@
+public class Onibus {
+    String nome;
+    String linha;
+}
+
+class Teste {
+    public static void main(String args) {
+        Onibus o = new Onibus();
+        o.linha = "Cotia-Butantã"
+    }
+}
